d037ed71159cb84100adf4b1a791e5780ff869f7
Andirapauline <55824902+Andirapauline@users.noreply.github.com>
2020-09-23 10:29:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,25 +12,34 @@
   
   <style>
   .bg-1 {
-    background-color: #ADD8E6; /* Lavender */
-    color: #B0C4DE; /* Light Steel Blue */
+    background-color: #B0C4DE ;  /* Light Steel Blue */
+    color: #ADD8E6; /* Lavender */
   }
   </style>
+  .bg-2 {
+    background-color: #B0C4DE ; /* Light Steel Blue */
+    color: #ffffff;
+  }
 </head>
 <body>
-
+<style>
+.container-fluid {
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+</style>
   <div class="container-fluid bg-1 text-center">
   <h3>Annyeonghaseyo Yeorobun</h3>
-  <img src="WFH.jpg" class="img-circle" alt="WFH">
+  <img src="WFH.jpg" alt="WFH" width="304" height="236">
   <h3>I'm an adventurer</h3>
 </div>
+  
   <div class="container-fluid bg-2 text-center">
   <h3>SUASANA WORK FROM HOME</h3>
   <p>Assalamualaikum warahmatullahi wabarakatuh
 ini tulisan saya mengenai suasana work from home saya dirumah. WFH dimulai sejak awal masuk disemester 4, tepat 2 minggu
 setelah kuliah tatap muka dilakukan. seperti yang kita ketahui juga kita melakukan WFH ini karena Indonesia tengah dilanda
-pandemi Covid-19.
-jujur awal WFH saya kaget mungkin karena belum terbiasa, mulai dari mempersiapkan aplikasi apa aja yang digunakan untuk WFH
+pandemi Covid-19. jujur awal WFH saya kaget mungkin karena belum terbiasa, mulai dari mempersiapkan aplikasi apa aja yang digunakan untuk WFH
 dan kuota internet yang ekstra. sejak WFH pun mengerjakan tugas ataupun diskusi sama teman jadi tidak bisa leluasa, dan selama 
 WFH jujur saja materi yang disampaikan kurang bisa dicerna otak karena dirumah pasti pikiran akan terbagi antara pekerjaan 
 rumah dan kuliah. tetapi saya bersyukur dikelilingi teman-teman yang baik, yang mau membantu sampai mengajari ulang bagian 
