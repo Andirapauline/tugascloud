596729af3546aaba78b6e296b969854acde52647
Andirapauline <55824902+Andirapauline@users.noreply.github.com>
2020-09-23 10:20:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,33 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <style>
+  .bg-1 {
+    background-color: #ADD8E6; /* Lavender */
+    color: #B0C4DE; /* Light Steel Blue */
+  }
+  </style>
 </head>
 <body>
 
-  <div class="container-fluid">
-<h3>Annyeonghaseyo Yeorobun</h3>
-<img src="WFH.jpg" alt="WFH">
-<h3>Andira Pauline</h3>
-  </div>
+  <div class="container-fluid bg-1 text-center">
+  <h3>Annyeonghaseyo Yeorobun</h3>
+  <img src="WFH.jpg" class="img-circle" alt="WFH">
+  <h3>I'm an adventurer</h3>
+</div>
+  <div class="container-fluid bg-2 text-center">
+  <h3>SUASANA WORK FROM HOME</h3>
+  <p>Assalamualaikum warahmatullahi wabarakatuh
+ini tulisan saya mengenai suasana work from home saya dirumah. WFH dimulai sejak awal masuk disemester 4, tepat 2 minggu
+setelah kuliah tatap muka dilakukan. seperti yang kita ketahui juga kita melakukan WFH ini karena Indonesia tengah dilanda
+pandemi Covid-19.
+jujur awal WFH saya kaget mungkin karena belum terbiasa, mulai dari mempersiapkan aplikasi apa aja yang digunakan untuk WFH
+dan kuota internet yang ekstra. sejak WFH pun mengerjakan tugas ataupun diskusi sama teman jadi tidak bisa leluasa, dan selama 
+WFH jujur saja materi yang disampaikan kurang bisa dicerna otak karena dirumah pasti pikiran akan terbagi antara pekerjaan 
+rumah dan kuliah. tetapi saya bersyukur dikelilingi teman-teman yang baik, yang mau membantu sampai mengajari ulang bagian 
+dari materi yang saya tidak mengerti.</p>
+</div>
   
 </body>
 </html>
