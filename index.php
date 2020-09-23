450cbfd49fe2e4f4bc9e92ebeab0c4a1d2073f16
Andirapauline <55824902+Andirapauline@users.noreply.github.com>
2020-09-23 10:08:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
   <title>Tugas Cloud Andira</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
+  <div class="container-fluid">
 <h3>Annyeonghaseyo Yeorobun</h3>
 <img src="WFH.jpg" alt="WFH">
 <h3>Andira Pauline</h3>
-
+  </div>
+  
 </body>
 </html>
