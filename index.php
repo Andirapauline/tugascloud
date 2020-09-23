531a3f0369cf1915bf1887d6a80a57523c928a64
Andirapauline <55824902+Andirapauline@users.noreply.github.com>
2020-09-23 11:35:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
@@ -9,6 +10,14 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+  font: 20px "Montserrat", sans-serif;
+  line-height: 1.8;
+  color: #f5f6f7;
+}
+</style>
+p {font-size: 16px;}
   
   <style>
   .bg-1 {
@@ -129,6 +138,9 @@ dari materi yang saya tidak mengerti.</p>
   Instagram & Twitter
   <br>
   Andirapauline
+  <br>
+  :)
+  </br>
   </br>
   </br>
 </footer>
