3c93a076b4a74d094dd744ac5ae013cd6e8966ec
Andirapauline <55824902+Andirapauline@users.noreply.github.com>
2020-09-24 11:32:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -103,29 +103,29 @@ p {font-size: 14px;}
     <br>
   <h3>Suasana Work From Home</h3>
     </br>
-<p><br>Assalamualaikum warahmatullahi wabarakatuh</br>
+<p><br>Assalamualaikum Warahmatullahi Wabarakatuh</br>
 <br>
-pengalaman saya selama kuliah dari rumah atau Work from home (WFH) ada menarik dan tidaknya ya, kalau menurut saya menariknya karena bisa dirumah setelah kuliah bisa langsung beristirahat. selain itu wfh dirumah juga menyenangkan karena bisa mengerjakan pekerjaan rumah dan tugas kuliah sekaligus, dengan wfh ini membagi waktu menjadi sangat berarti dan berguna karena mengejakan
-beberapa tugas sekaligus, misalnya setelah kuliah selesai saya bisa mengerjakan tugas rumah terlebih dulu seperti beres-beres rumah, ya tugas anak perempuan pada umumnya.
-lalu tugas kuliah saya kerjakan biasanya tugas yang lebih dahulu mau dikumpulkan biasanya sih mengerjakan tugas malam hari bahkan bisa sampai larut malam untuk tugas yang waktunya dekat dengan waktu pengumpulan.
-mengerjakan tugas-tugas juga saya biasa berdiskusi dengan teman kelompok, karena sedang wfh jadi diskusi secara daring lewat zoom atau google meet.
+Pengalaman saya selama kuliah dari rumah atau Work from home (WFH) tentu ada menarik dan tidaknya, kalau menurut saya menariknya WFH ini karena bisa tetap dirumah bahkan setelah kuliah selesai bisa langsung beristirahat. Selain itu WFH juga menyenangkan karena bisa mengerjakan pekerjaan rumah dan tugas kuliah sekaligus, dengan WFH ini saya belajar untuk membagi waktu dan waktu sangat berguna karena mengerjakan
+beberapa tugas sekaligus, misalnya setelah kuliah selesai saya bisa mengerjakan pekerjaan rumah terlebih dulu seperti beres-beres rumah dan lainnya seperti tugas anak perempuan pada umumnya.
+Lalu tugas kuliah saya kerjakan biasanya tugas yang lebih dahulu mau dikumpulkan, biasanya sih saya mengerjakan tugas waktu malam hari bahkan bisa sampai larut malam untuk tugas yang waktunya dekat dengan waktu pengumpulan.
+Mengerjakan tugas-tugas juga saya biasa berdiskusi dengan teman kelompok, karena sedang WFH jadi diskusi dilakukan secara daring lewat zoom atau google meet.
 </br>
 <br>
-liburan saya saat wfh juga cukup menyenangkan karena tidak bisa keluar rumah jadi biasanya sabtu-minggu saat libur kuliah saya gunakan untuk hobi saya, seperti mengerjakan projek tulisan cerita saya,
-menonton drama korea, dan biasanya dihalaman rumah saya berolahraga bareng adik-adik memainkan olahraga bulutangkis.
-selama wfh juga saya bisa membantu adik-adik saya mengerjakan tugas dari sekolahnya dan mengajarkan materi pelajaran yang mereka tidak pahami.
+Liburan saya saat WFH juga cukup menyenangkan karena tidak bisa keluar rumah jadi biasanya sabtu-minggu saat libur kuliah saya gunakan untuk hobi saya, seperti mengerjakan projek tulisan cerita,
+menonton drama korea dan biasanya dihalaman rumah saya berolahraga dengan adik-adik memainkan olahraga bulutangkis.
+Selama WFH juga saya dapat membantu adik-adik saya mengerjakan tugas dari sekolahnya dan mengajarkan materi pelajaran yang mereka tidak pahami.
 </br>
 <br>
-kurang lebih sudah 9 bulan wfh jujur saja tidak menariknya wfh mungkin bosan karena mengerjakan aktifitas yang monoton dirumah, tidak bisa bertemu dan kumpul dengan teman-teman, apalagi saya belum sempat untuk
-mengikuti organisasi dikampus. saya juga sudah rindu teman-teman kelas, rindu lingkungan kampus juga. saya berharap semester depan sudah bisa normal kembali.</br></p>
+Kurang lebih sudah 9 bulan WFH, jujur saja tidak menariknya WFH mungkin bosan karena melakukan aktifitas yang monoton dirumah setiap hari, tidak bisa bertemu dan kumpul dengan teman-teman, apalagi saya belum sempat untuk
+mengikuti organisasi dikampus. saya juga sudah rindu dengan teman-teman kelas, rindu lingkungan kampus juga. Saya berharap semester depan sudah bisa normal kembali.</br></p>
 </div>
   
  <div class="container-fluid bg-3 text-center">    
   <h3>Fasilitas UIN Syarif Hidayatullah Jakarta</h3><br>
   <div class="row">
     <div class="col-sm-4">
-      <p>Fasilitas utama menurut saya fakultas, ini gambar fakultas sains dan teknologi. selain terdapat ruang kelas sebagai tempat belajar didalam fakultas ini juga banyak fasilitas pendukung lain misalnya,
-      mushollah, kantin, terdapat juga gazebo ditengah fakultas biasanya disebut saung. lobby fakultas juga dimanfaatkan mahasiswa untuk berkumpul, mengobrol sampai mengerjakan tugas</p>
+      <p>Fasilitas utama menurut saya yaitu fakultas, ini gambar fakultas sains dan teknologi. Selain terdapat ruang kelas sebagai tempat belajar didalam fakultas ini juga banyak fasilitas pendukung lain misalnya,
+      mushollah, kantin dan terdapat juga gazebo yang letaknya ditengah fakultas biasanya juga disebut saung. Lobby fakultas juga dimanfaatkan mahasiswa untuk berkumpul, mengobrol sampai mengerjakan tugas</p>
       <br>
       <br>
       <br>
@@ -139,8 +139,8 @@ mengikuti organisasi dikampus. saya juga sudah rindu teman-teman kelas, rindu li
 </br>
     </div>
     <div class="col-sm-4"> 
-      <p>Fasilitas lain dari UIN Jakarta yaitu Perpustakaan. Perpustakaan UIN Jakarta sangat luas dan tersedia berbagai macam buku sampai kekumpulan skripsi mahasiswa, selain untuk tempat belajar dan mencari buku
-      biasanya perpustakaan bekerja sama dengan gramedia mengadakan bazar buku murah dan alat-alat tulis. suasana diperpustakaan sangat nyaman dan hening cocok sebagai tempat mengerjakan tugas</p>
+      <p>Fasilitas lain dari UIN Jakarta yaitu Perpustakaan. Perpustakaan UIN Jakarta sangat luas dan tersedia berbagai macam buku sampai sekumpulan skripsi mahasiswa, selain untuk tempat belajar dan mencari buku
+      biasanya perpustakaan bekerjasama dengan Gramedia atau toko buku lainnya untuk mengadakan bazar buku murah dan alat-alat tulis. Suasana diperpustakaan sangat nyaman dan hening cocok sebagai tempat mengerjakan tugas</p>
      <br>
       <br>
       <br> 
@@ -152,9 +152,9 @@ mengikuti organisasi dikampus. saya juga sudah rindu teman-teman kelas, rindu li
 </br>
     </div>
     <div class="col-sm-4"> 
-      <p>Fasilitas yang salah satunya baik menurut saya yaitu RS UIN. karena kampus memiliki rumah sakit sendiri digunakan untuk warga kampus maupun masyarakat diluar kampus. sepengelaman saya menggunakan
-      fasilitas ini juga mudah dan nyaman, bagi saya yang seorang mahasiswa untuk berobat ke RS UIN ini cukup menggunakan kartu mahasiswa dan menunjukan slip pembayaran semester, lalu saya mengatakan akan menemui dokter sesuai keluhan.
-      setelah itu hanya menunggu gilliran masuk untuk diperiksa dokter dan sesudahnya hanya tinggal menunggu resep obat dari apoteknya.</p>
+      <p>Fasilitas yang salah satunya baik menurut saya yaitu RS UIN. karena kampus memiliki rumah sakit sendiri yang digunakan untuk warga kampus maupun masyarakat diluar kampus. Sepengalaman saya menggunakan
+      fasilitas ini juga sangat mudah dan nyaman, bagi saya yang seorang mahasiswa untuk berobat ke RS UIN ini cukup menggunakan kartu mahasiswa dan menunjukan slip pembayaran semester terakhir, lalu saya mengatakan akan menemui dokter sesuai keluhan.
+      Setelah itu hanya menunggu giliran masuk untuk diperiksa dokter dan sesudahnya hanya tinggal menunggu resep obat dari apoteknya.</p>
       <img src="rsuin.jpg" class="img-rounded" alt="WFH" width="350" height="220" alt="rsuin">
     </div>
   </div>
